5f68b4b4a25a583ce1867fbf7c13f6e3d4203ec4
Sarthak Trivedi <sarthaktrivedi386@gmail.com>
2026-01-17 01:27:50 +0530

    UPDATE: auth.controller.js, added the try block code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,42 @@
 import mongoose from "mongoose";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {JWT_EXPIRES_IN, JWT_SECRET} from "../config/env.js";
+
+// what is a req.body -> an object coming from the client that contains data specially in a post request
 
 export const signUp = async (req, res, next) => {
     const session = await mongoose.startSession();
-    session.startSession();
+    session.startTransaction();
 
     try {
+        //     create a new user
+        const {name, email, password} = req.body;
+
+        // check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const error = new Error("User already exists");
+            error.statusCode = 409;
+            throw error;
+        }
+
+        //   Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const newUsers = await User.create([{name, email, hashPassword}], {session});
+
+        const token = jwt.sign(newUsers[0], JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
 
         await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            data: {token, user: newUsers[0]},
+        })
 
     } catch (error) {
         await session.abortTransaction();
